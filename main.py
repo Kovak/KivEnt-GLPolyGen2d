9002912bacd0ba1a3b909b2dff797d6f14f30a3f
Kovak <kovac1066@gmail.com>
2014-04-30 23:18:35 -0600

    clean up from old method

--- main.py
+++ main.py
@@ -34,57 +34,18 @@ class RegularPolygonRenderer(Widget):
             ('vColor', 4, 'float'),
             ]
         angle = 2 * pi / sides
-        vertices = []
-        indices = []
-        ie = indices.extend
-        ex = vertices.extend
-        #Add the center point and its color to get started
-        ex(pos)
-        ex(middle_color)
         all_verts = []
         all_verts_a = all_verts.append
         all_verts_a(pos)
-        vert_dict = {}
         r_total = 0
         i = 0
-        all_verts = []
-        all_vert_a = all_verts.append
         for count in range(levels):
-            
             level = i + 1
-            print i, level
-            vert_dict[level] = verts = []
             r, color = radius_color_dict[level]
-            vert_a = verts.append
             for s in range(sides):
-                vert_a((x + (r + r_total) * sin(s * angle), 
-                    y + (r + r_total) * cos(s * angle)))
-                all_vert_a((x + (r + r_total) * sin(s * angle), 
+                all_verts_a((x + (r + r_total) * sin(s * angle), 
                     y + (r + r_total) * cos(s * angle)))
             r_total +=  r
-            c = 1 #side number we are on in loop
-            if level == 1:
-                for each in verts:
-                    ex(each)
-                    ex(color)
-                    if c < level*sides:
-                        ie([c, 0, c+1])
-                    else:
-                        ie([c, 0, 1])
-                    c += 1
-            else:
-                for each in verts:
-                    ex(each)
-                    ex(color)
-                    offset = sides*(i-1)
-                    if c < sides:
-                        ie([c+sides+offset, c+sides+1+offset, c+offset, 
-                            c+offset, c+1+offset, c+sides+1+offset])
-                    else:
-                        ie([c+sides+offset, sides+1+offset, sides+offset, 
-                            sides+offset, 1+offset, sides+1+offset])
-                    c += 1
-            i += 1
         A = {'vertices':array(all_verts)}
         B = triangle.triangulate(A, 'qa200.')
         tri_indices = B['triangles']
